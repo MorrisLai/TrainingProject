3f6a0dead69d4c0c249f5342be9264deb96ff9f6
MorrisLai <iamkim1127@gmail.com>
2015-12-16 22:40:54 +0800

    Update memberUpdate.apsx.cs

--- memberUpdate.apsx.cs
+++ memberUpdate.apsx.cs
@@ -23,13 +23,6 @@ public partial class Member_Update : System.Web.UI.Page
     }
     protected void FormView1_ItemUpdating(object sender, FormViewUpdateEventArgs e)
     {
-        //Label Mem_Pwd= (Label)FormView1.FindControl("Mem_Pwd");
-        //Label Mem_Nation= (Label)FormView1.FindControl("Mem_Nation");
-        //Label Mem_City=     (Label)FormView1.FindControl("Mem_City");
-        //Label Mem_Mobile = (Label)FormView1.FindControl("Mem_Mobile");
-        //e.NewValues["Mem_Pwd"] = Mem_Pwd.Text;
-        //e.NewValues["Mem_Nation"] = Mem_Nation.Text;
-        //e.NewValues["Mem_City"] = Mem_City.Text;
-        //e.NewValues["Mem_Mobile"] = Mem_Mobile.Text;
+        
     }
 }
